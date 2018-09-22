8a44a6b4a7b87a35741274670809341ef5d27681
markerhz <42089132+markerhz@users.noreply.github.com>
2018-09-22 15:20:21 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -5,10 +5,10 @@ DEVICE = 0x20
 IODIRA = 0x00 
 OLATA = 0x14 
 GPIOA = 0x12 
-bus.write_byte_data(DEVICE,IODIRA,0x00)
-bus.write_byte_data(DEVICE,OLATA,0)
+bus.write_byte_data
+bus.write_byte_data
 for MyData in range(1,8):
-	bus.write_byte_data(DEVICE,OLATA,MyData)
+	bus.write_byte_data
 	print(MyData) 
 	time.sleep(1) 
-	bus.write_byte_data(DEVICE,OLATA,0)
+	bus.write_byte_data
