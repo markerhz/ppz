1cabc7a8dd5c6e17f38792a98cdf7cc1f9ac1df8
markerhz <42089132+markerhz@users.noreply.github.com>
2018-09-22 15:08:13 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -8,7 +8,7 @@ GPIOA = 0x12
 bus.write_byte_data
 bus.write_byte_data
 for MyData in range(1,8):
-	bus.write_byte_data
+	bus.write_byte_data(DEVICE,OLATA,MyData)
 	print(MyData) 
 	time.sleep(1) 
-	bus.write_byte_data
+	bus.write_byte_data(DEVICE,OLATA,0)
